01765e301dab409bf4f15460c5f9a057d00e39cc
Attila Nagy <attilanagy85@gmail.com>
2017-09-20 15:30:27 +0200

    Using the good old nerdtree pluggin

--- vim/install_vim_plugins.sh
+++ vim/install_vim_plugins.sh
@@ -18,8 +18,8 @@ git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
 git clone https://github.com/garbas/vim-snipmate.git
 git clone https://github.com/honza/vim-snippets.git
 
-# vim-nerdtree-tabs
-git clone https://github.com/jistr/vim-nerdtree-tabs
+# NerdTree
+git clone https://github.com/scrooloose/nerdtree
 
 # NerdCommenter
 git clone https://github.com/ddollar/nerdcommenter
