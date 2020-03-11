336434fa658d73889b97cd85f47569860be6f2bc
Attila <attila@example.com>
2020-03-11 10:40:12 +0100

    adding basic install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sudo apt-get install vim zsh git curl ranger
+
+ln -s ~/.zshrc ~/.zshrc || exit
+chsh -s /bin/zsh || exit
+
+ln -s $DIR/.vimrc ~/.vimrc || exit
+
+test -d ~/.vim && echo .vim already exist && exit
+bash $DIR/vim/install_vim_plugins.sh
+
